0ed33e240316bd6e49dd238452055b4b97351d76
Marek <maswiecho@gmail.com>
2023-07-26 10:33:44 +0200

    react native eject + gdradle wrapper edit

---
+++ android/app/src/main/java/com/supercard/MainActivity.java
@@ -0,0 +1,15 @@
+package com.supercard;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "Supercard";
+    }
+}
